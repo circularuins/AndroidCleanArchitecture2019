1db91fb01be09c75d60f4c79fa7dd07effccfe85
circularuins <natsuhiko.wake@gmail.com>
2019-07-09 21:43:44 +0900

    main画面でのpresenterを通した通信

--- app/src/main/java/com/circularuins/simplemercari/app/MainActivity.kt
+++ app/src/main/java/com/circularuins/simplemercari/app/MainActivity.kt
@@ -5,16 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import com.circularuins.simplemercari.MercariApplication
 import com.circularuins.simplemercari.R
 import com.circularuins.simplemercari.domain.model.Master
-import com.circularuins.simplemercari.domain.repository.ItemRepository
 import com.circularuins.simplemercari.domain.repository.MasterRepository
+import com.circularuins.simplemercari.domain.usecase.MainUseCase
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), MainContract.View {
 
     @Inject
-    lateinit var masterRepository: MasterRepository
-    @Inject
-    lateinit var itemRepository: ItemRepository
+    lateinit var repository: MasterRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,14 +20,9 @@ class MainActivity : AppCompatActivity(), MainContract.View {
 
         (application as MercariApplication).component.inject(this)
 
-        // TODO: 消す
-        itemRepository.getItems("women")
-            .subscribe({
-                val hoge = it.get(0)
-                print(hoge.name)
-            }, {
-                print(it)
-            })
+        // TODO: presenter生成もDaggerで
+        val presenter = MainPresenter(this, MainUseCase(repository))
+        presenter.start()
     }
 
     override fun showProgress() {
